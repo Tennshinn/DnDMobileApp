9520d6be7acdcc53d6331610dedd124ece8be77d
piotrszleg <szlegp@gmail.com>
2021-11-07 22:09:14 +0100

    ItemView, moved styles to a separate file.

--- ItemGrid.js
+++ ItemGrid.js
@@ -1,10 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { View, Image, StyleSheet, Text } from 'react-native';
+import React from 'react';
+import { View, Image, Text } from 'react-native';
 import { FlatGrid } from 'react-native-super-grid';
-
-const PRIMARY = "#efd3ad";
-const BACKGROUND = "#010101";
-const PANEL = "#121212";
+import styles from "./styles";
 
 const Item = ({ text, image }) => {
   return (<View
@@ -26,45 +23,6 @@ const ItemGrid = () => {
     />
   );
 };
-
-const styles = StyleSheet.create({
-  body: {
-    backgroundColor: BACKGROUND,
-    height: "100%",
-  },
-  gridView: {
-    marginTop: 10,
-    flex: 1,
-  },
-  itemContainer: {
-    borderRadius: 5,
-    height: 150,
-
-
-    backgroundColor: PANEL,
-    padding: 7
-  },
-  itemName: {
-    fontSize: 16,
-    color: '#fff',
-    fontWeight: '600',
-  },
-  itemCode: {
-    fontWeight: '600',
-    fontSize: 12,
-    color: '#fff',
-  },
-  text: {
-    color: PRIMARY,
-    textAlign: "center"
-  },
-  image: {
-    width: '100%',
-    height: 100,
-    resizeMode: 'cover',
-    borderRadius: 5,
-  },
-});
 
 const Items = () => {
   return (<View style={styles.body}>
