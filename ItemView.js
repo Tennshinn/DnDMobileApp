c822b89da1a50f0174e7a351ad94d60317f85985
piotrszleg <szlegp@gmail.com>
2021-12-06 18:53:16 +0100

    Item view parameters.

--- ItemView.js
+++ ItemView.js
@@ -1,35 +1,36 @@
 import React, { useState, useEffect } from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, TouchableWithoutFeedback } from 'react-native';
 import styles from "./styles";
 
-const ItemView = () => {
+const ItemView = (props) => {
 
-  const [state, setState] = useState({
-    title:"Magic Health Potion", image:require('./img/fire-bowl.png') ,
-    description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates dolorem molestiae necessitatibus quod. Veniam facere nobis recusandae repudiandae iste voluptas minima aut repellendus nihil quis exercitationem quas, inventore iusto. Minima?", 
-    icons:"4⚅     2⚂"
-  });
+  const name = props.name ?? "Magic Health Potion";
+  const image = props.image ?? require('./img/fire-bowl.png');
+  const description = props.description ?? "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates dolorem molestiae necessitatibus quod. Veniam facere nobis recusandae repudiandae iste voluptas minima aut repellendus nihil quis exercitationem quas, inventore iusto. Minima?";
+  const icons = props.icons ?? "4⚅     2⚂";
 
   return (
   <View style={styles.body}>
-  <View
-    style={[styles.itemContainer, styles.itemView]}
-  >
-    <View style={{flexDirection:"row"}}>
-    <Image source={state.image} style={styles.itemViewImage}></Image>   
-    <Text
-      style={styles.itemTitle}
-    >{state.title}</Text>
-</View>
-
-    <Text
-      style={styles.itemDescription}
-    >{state.description}</Text>
-
-<Text
-      style={styles.itemIcons}
-    >{state.icons}</Text>
+    <TouchableWithoutFeedback onPress={props.onPress}>
+    <View
+      style={[styles.itemContainer, styles.itemView]}
+    >
+      <View style={{flexDirection:"row"}}>
+      <Image source={image} style={styles.itemViewImage}></Image>   
+      <Text
+        style={styles.itemTitle}
+      >{name}</Text>
   </View>
+
+      <Text
+        style={styles.itemDescription}
+      >{description}</Text>
+
+  <Text
+        style={styles.itemIcons}
+      >{icons}</Text>
+    </View>
+    </TouchableWithoutFeedback>
   </View>);
 }
 
