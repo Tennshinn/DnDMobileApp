34c1fbdd2173e0a753fc3a107a588bc46718a324
piotrszleg <szlegp@gmail.com>
2021-12-15 02:20:42 +0100

    Mixing random item colors with primary color.

--- data/ItemData.js
+++ data/ItemData.js
@@ -33,15 +33,54 @@ function hashString(s) {
   return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
 };
 
-export default class ItemData {
-    constructor(name, image, description, icons) {
-      this.name = name ?? "Health Potion";
-      this.image = image ?? require('../img/fire-bowl.png');
-      this.description = description ?? `${Math.random()} Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates dolorem molestiae necessitatibus quod. Veniam facere nobis recusandae repudiandae iste voluptas minima aut repellendus nihil quis exercitationem quas, inventore iusto. Minima?`;
-      this.icons = icons ?? parseDice("4d20    2d10");
+function lerp( a,  b,  lerpFactor)
+{
+  return ((1 - lerpFactor) * a) + (lerpFactor * b);
+}
 
-      const toHash = [this.name, this.description];
-      const hash = toHash.reduce((acc, curr)=>acc+hashString(curr), 0);
-      this.color = hslToHex(hash%360, 12, 65); // S and L were selected by trying different values 
+function lerpAngle(a, b, lerpFactor) // Lerps from angle a to b (both between 0.f and 360.f), taking the shortest path
+{
+    let result;
+    let diff = b - a;
+    if (diff < -180)
+    {
+        // lerp upwards past 360
+        b += 360;
+        result = lerp(a, b, lerpFactor);
+        if (result >= 360)
+        {
+            result -= 360;
+        }
     }
+    else if (diff > 180)
+    {
+        // lerp downwards past 0
+        b -= 360;
+        result = lerp(a, b, lerpFactor);
+        if (result < 0)
+        {
+            result += 360;
+        }
+    }
+    else
+    {
+        // straight lerp
+        result = lerp(a, b, lerpFactor);
+    }
+
+    return result;
+}
+
+export default class ItemData {
+  constructor(name, image, description, icons) {
+    this.name = name ?? "Health Potion";
+    this.image = image ?? require('../img/fire-bowl.png');
+    this.description = description ?? `${Math.random()} Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates dolorem molestiae necessitatibus quod. Veniam facere nobis recusandae repudiandae iste voluptas minima aut repellendus nihil quis exercitationem quas, inventore iusto. Minima?`;
+    this.icons = icons ?? parseDice("4d20    2d10");
+
+    const toHash = [this.name, this.description];
+    const hash = toHash.reduce((acc, curr)=>acc+hashString(curr), 0);
+    // 35 comes from PRIMARY_COLOR, S and L were selected by trying different values 
+    this.color = hslToHex(lerpAngle(hash%360, 35, 0.66), 15, 60);     
   }
+}
